0cfa57bc7a7cf7c85c91b69bed5c522791226c73
LucasChanquia <lucaschanquia@gmail.com>
2023-08-30 14:56:28 -0300

    Implemented login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image";
+import LogoFoccus from 'public/logos/LogoFoccus.png'
+import style from './page.module.scss'
+
 export default function Home() {
-	return <main />
+	return <main id={style.loginPage}>
+		<section className={style.banner} >
+
+			<blockquote>“Observa el mundo con ojos nuevos, enfocate en lo que realmente importa.”</blockquote>
+
+
+		</section>
+		<section className={style.right}>
+			<Image alt="Logo Foccus" src={LogoFoccus} />
+			<article className={style['form-container']}>
+				<h2>Inicia Sesion en FOCCUS</h2>
+				<form>
+					<label>Ingrese su usuario
+						<input type="text" placeholder="oliviaaroud123" />
+					</label>
+					<label>Ingrese su contraseña
+						<input type="password" placeholder="*******" />
+					</label>
+					<input type="submit" value='Iniciar Sesion' />
+				</form>
+				<div>
+					<h3>ALGUN PROBLEMA?</h3>
+					<button type="button">Solicitar soporte</button>
+					<button type="button">Resetear contraseña</button>
+
+				</div>
+			</article>
+		</section>
+
+	</main>
 }
