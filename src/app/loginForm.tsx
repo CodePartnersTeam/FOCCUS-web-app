5dad080fe0fbfa954b09c3bf0e9ae55db20295f7
devEstivenValencia <dev@estivenvalencia.co>
2023-09-05 13:20:32 -0500

    Applied formatting

--- src/app/loginForm.tsx
+++ src/app/loginForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import React, { useState }  from 'react'
 import style from './page.module.scss'
 import { Credentials } from '@/models'
 
@@ -10,7 +10,8 @@ export default function LoginForm() {
 	})
 
 	const handlerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setLoginData((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+		setLoginData(prev => ({ ...prev, [e.target.name]: e.target.value }))
+		console.log(loginData);
 	}
 
 	return (
@@ -19,23 +20,23 @@ export default function LoginForm() {
 			<form>
 				<label>
 					Ingrese su usuario
-					<input type="text" name="username" placeholder="oliviaaroud123" autoFocus={true} onChange={handlerChange} />
+					<input type='text' name='username' placeholder='oliviaaroud123' autoFocus={true} onChange={handlerChange} />
 				</label>
 				<label>
 					Ingrese su contraseña
-					<input type="password" name="password" placeholder="******* " onChange={handlerChange} />
+					<input type='password' name='password' placeholder='******* ' onChange={handlerChange} />
 				</label>
-				<input type="submit" value="Iniciar Sesion" />
+				<input type='submit' value='Iniciar Sesion' />
 			</form>
 			<div className={style.actions}>
 				<h3>
-					<span className="decorator line" />
+					<span className='decorator line' />
 					ALGUN PROBLEMA?
-					<span className="decorator line" />
+					<span className='decorator line' />
 				</h3>
 				<div className={style.bntActions}>
-					<button type="button">Solicitar soporte</button>
-					<button type="button">Resetear contraseña</button>
+					<button type='button'>Solicitar soporte</button>
+					<button type='button'>Resetear contraseña</button>
 				</div>
 			</div>
 		</article>
