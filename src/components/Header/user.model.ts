20c42520ac2a7411d7fbdb5de5af942f0e4dfde7
devEstivenValencia <dev@estivenvalencia.co>
2023-09-12 08:57:35 -0500

    Implemented Header component (User Actions, Menu nav)

---
+++ src/components/Header/user.model.ts
@@ -0,0 +1,11 @@
+interface userActionsTypes {
+	title: string
+	icon: string
+}
+
+const userActions: Array<userActionsTypes> = [
+	{ title: 'Contactar soporte', icon: 'phone.svg' },
+	{ title: 'Cerrar sesion', icon: 'out.svg' }
+]
+
+export { userActions }
