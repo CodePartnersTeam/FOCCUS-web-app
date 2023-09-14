5cbe36747c6059f30f67a5627c9b6a4b9216c864
Emiliano Tancredi <111023349+EmilianoTancredi21@users.noreply.github.com>
2023-09-14 18:02:04 -0300

    Implemented Toast component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Header } from '@/components/Header'
+import ToasterComponent from '@/components/Toaster'
 import { inter, montserrat, montserratAlternates } from '@/fonts'
 import '@/styles/global.scss'
 import '@/styles/index.scss'
@@ -9,6 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang='es' className={`${inter.variable} ${montserrat.variable} ${montserratAlternates.variable}`}>
 			<body>
+				<ToasterComponent />
 				<Header />
 				{children}
 			</body>
