91b324d3e8dcc0a517f7e4132df4092a6fc37254
devEstivenValencia <dev@estivenvalencia.co>
2023-09-15 11:05:15 -0500

    Implemented view with logout button

--- src/app/vision_general/page.tsx
+++ src/app/vision_general/page.tsx
@@ -1,61 +1,22 @@
 'use client'
+
 import React from 'react'
-import useSWR from 'swr'
 import { toast } from 'sonner'
-import { Credentials } from '@/models'
+import { useRouter } from 'next/navigation'
+import { logout } from '@/services/Login'
 
-const fetcher = async (url: string, credentials: Credentials) => {
-	const res = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'content-type': 'application/json'
-		},
-		body: JSON.stringify(credentials)
-	})
+export default function Page() {
+	const router = useRouter()
 
-	if (!res.ok) {
-		let errors = {}
-		errors = new Error('An error occurred while fetching the data.')
-		errors = await res.json()
-		throw errors
-	}
-
-	return await res.json()
-}
-
-const useUser = () => {
-	const { data, error, isLoading } = useSWR(
-		[
-			'https://foccus-back-dashboard-production.up.railway.app/auth/login',
-			{
-				username: 'LuisCarlos',
-				password: 'iaASA123#'
-			}
-		],
-		([url, credentials]) => fetcher(url, credentials),
-		{
-			revalidateOnMount: false
-		}
+	return (
+		<button
+			onClick={async () => {
+				const { message } = await logout()
+				router.push('/')
+				toast(message)
+			}}
+		>
+			Cerrar Sesion
+		</button>
 	)
-
-	return {
-		data,
-		isLoading,
-		error
-	}
-}
-
-export default function App() {
-	const User: any = () => useUser()
-	const { data, isLoading, error } = User()
-
-	const handleClick = async () => {
-		console.log(data, isLoading, error)
-	}
-
-	if (isLoading) return <div>loading...</div>
-	if (error) toast.error(`Error: ${error.message}`)
-	if (data) toast.success(`Bienvenido! ${data.userFound.name}`)
-
-	return <button onClick={() => handleClick}>Click</button>
 }
