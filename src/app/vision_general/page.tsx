b47d0af20a6cc94df3e84733938e01bccb83ee37
devEstivenValencia <dev@estivenvalencia.co>
2023-09-24 22:23:41 -0500

    Reset page

--- src/app/vision_general/page.tsx
+++ src/app/vision_general/page.tsx
@@ -1,22 +1,3 @@
-'use client'
-
-import React from 'react'
-import { toast } from 'sonner'
-import { useRouter } from 'next/navigation'
-import { logout } from '@/services/Login'
-
 export default function Page() {
-	const router = useRouter()
-
-	return (
-		<button
-			onClick={async () => {
-				const { message } = await logout()
-				router.push('/')
-				toast(message)
-			}}
-		>
-			Cerrar Sesion
-		</button>
-	)
+	return <></>
 }
