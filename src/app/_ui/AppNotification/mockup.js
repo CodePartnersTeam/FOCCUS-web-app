701d0d2978d0283c12a34980312bed98d573ceb5
DiegoS <sdiegoss@gmail.com>
2023-11-18 15:52:13 +0100

    chore(Notification Item): :clown_face: Mockup of notification info to render into the component NotificationItem

---
+++ src/app/_ui/AppNotification/mockup.js
@@ -0,0 +1,72 @@
+export const userNoti = [
+	{
+		timestamp: '2023-11-14T16:14:15.726Z',
+		title: 'Notification 1',
+		description: 'This is notification 1',
+		ticket: '1',
+		seen: true
+	},
+	{
+		timestamp: '2023-11-15T06:50:42.109Z',
+		title: 'Notification 2',
+		description: 'This is notification 2',
+		ticket: '2',
+		seen: false
+	},
+	{
+		timestamp: '2023-11-25T17:03:48.567Z',
+		title: 'Notification 3',
+		description: 'This is notification 3',
+		ticket: '3',
+		seen: ''
+	},
+	{
+		timestamp: '2023-11-05T08:30:45.123Z',
+		title: 'Notification 4',
+		description: 'This is notification 4',
+		ticket: '4',
+		seen: true
+	},
+	{
+		timestamp: '2023-11-18T19:22:12.456Z',
+		title: 'Notification 5',
+		description: 'This is notification 5',
+		ticket: '5',
+		seen: ''
+	},
+	{
+		timestamp: '2023-11-01T14:10:30.789Z',
+		title: 'Notification 6',
+		description: 'This is notification 6',
+		ticket: '6',
+		seen: false
+	},
+	{
+		timestamp: '2021-01-07',
+		title: 'Notification 7',
+		description: 'This is notification 7',
+		ticket: '7',
+		seen: false
+	},
+	{
+		timestamp: '2021-01-08',
+		title: 'Notification 8',
+		description: 'This is notification 8',
+		ticket: '8',
+		seen: false
+	},
+	{
+		timestamp: '2021-01-09',
+		title: 'Notification 9',
+		description: 'This is notification 9',
+		ticket: '9',
+		seen: true
+	},
+	{
+		timestamp: '2021-01-10',
+		title: 'Notification 10',
+		description: 'This is notification 10',
+		ticket: '10',
+		seen: true
+	}
+]
