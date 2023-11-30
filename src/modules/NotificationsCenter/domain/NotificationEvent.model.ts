24f14c345c3220868e85276ccf844d13de1146a0
devEstivenValencia <dev@estivenvalencia.co>
2023-11-30 13:04:42 -0500

    feat: :sparkles: entidad de NotificationCenter

---
+++ src/modules/NotificationsCenter/domain/NotificationEvent.model.ts
@@ -0,0 +1,22 @@
+interface ResponseNotificationEvent {
+	message: string
+}
+
+export interface NotificationEventDTO {
+	_id: string
+	userId: string
+	category: string
+	response: ResponseNotificationEvent
+	location: string
+	delivered: boolean
+	seen: boolean
+	createdAt: Date
+}
+
+export interface NotificationEvent {
+	ticket: string
+	category: string
+	seen: boolean
+	timestamp: Date
+	data: ResponseNotificationEvent
+}
