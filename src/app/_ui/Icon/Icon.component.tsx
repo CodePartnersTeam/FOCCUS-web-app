11214077efb15c61239a6f07e84a290e2935994f
devEstivenValencia <dev@estivenvalencia.co>
2023-11-30 13:01:20 -0500

    feat: :sparkles: añadido componente para la incorporacion rapida de iconos

---
+++ src/app/_ui/Icon/Icon.component.tsx
@@ -0,0 +1,15 @@
+import cn from 'classnames'
+import Image from 'next/image'
+
+interface IconProps {
+	url: string
+	alt: string
+	height?: number
+	width?: number
+	classNames?: string
+}
+
+// TODO: Meter un fallback para cuando no se encuentra un Icon
+export function Icon({ url, alt, height = 20, width = 20, classNames }: IconProps) {
+	return <Image width={width} height={height} alt={alt} className={cn(classNames)} src={url} />
+}
