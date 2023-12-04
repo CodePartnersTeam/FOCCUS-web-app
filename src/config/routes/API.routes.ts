d4c5722ef3c7d5d2aa7a814f64da5f5aaf718848
devEstivenValencia <dev@estivenvalencia.co>
2023-12-04 08:00:48 -0500

    chore: :truck: cambiada la URI

--- src/config/routes/API.routes.ts
+++ src/config/routes/API.routes.ts
@@ -1,5 +1,5 @@
 import { KeyWithValue } from '@/common/types'
 
-export const APIURL: string = 'https://api-foccus.up.railway.app/'
+export const APIURL: string = 'https://api-foccus.up.railway.app'
 
 export const APIROUTES: KeyWithValue<string> = {}
