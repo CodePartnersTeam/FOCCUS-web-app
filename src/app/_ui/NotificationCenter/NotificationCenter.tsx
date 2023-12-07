15b0a4b07c085832e343de594c7aa083a8289a64
devEstivenValencia <dev@estivenvalencia.co>
2023-12-07 08:35:33 -0500

    feat: :sparkles: Se suscribe al sse una vez se monta la app

---
+++ src/app/_ui/NotificationCenter/NotificationCenter.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { launchNotificacionsToast } from '@/app/_ui/NotificationCenter/notificationToast.launch'
+import { getAuthorization } from '@/common/utils'
+import { onReceivingMessage, saveNotificationEventsToLS } from '@/modules/NotificationsCenter/infrastructure'
+import React from 'react'
+import { SSE } from 'sse.js'
+
+export function NotificationCenter({ children }: { children: React.ReactNode }) {
+	const MAX_RECONNECT_TRIES = 3
+	let RECONNECT_ATTEPMS = 0
+	const dateOfLastNotification = localStorage.getItem('dateOfLastNotificationEvent')
+
+	const conection = new SSE(`https://api-foccus.up.railway.app/sse/event/${dateOfLastNotification || 0}`, {
+		headers: { Authorization: getAuthorization() }
+		// debug: true
+	})
+
+	conection.onmessage = event => {
+		const { newNotifications, notificationsToSave } = onReceivingMessage(JSON.parse(event.data))
+		const lastnotificationDate = newNotifications[0].timestamp
+		launchNotificacionsToast(newNotifications)
+		saveNotificationEventsToLS(notificationsToSave)
+		localStorage.setItem('dateOfLastNotificationEvent', String(lastnotificationDate))
+	}
+
+	conection.onabort = () => {
+		console.log('Conexion con el servidor cerrada')
+	}
+
+	conection.onerror = () => {
+		if (RECONNECT_ATTEPMS > MAX_RECONNECT_TRIES) {
+			conection.close()
+		} else {
+			console.log('Reconectando...')
+			RECONNECT_ATTEPMS++
+		}
+	}
+
+	window.addEventListener('beforeunload', () => {
+		conection.close()
+	})
+
+	return children
+}
--- src/config/providers/_provider.tsx
+++ src/config/providers/_provider.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import { NotificationsProvider } from '.'
+import { NotificationCenterProvider } from './NotificationCenter.provider'
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<NotificationsProvider />
-			{children}
+			<NotificationCenterProvider>{children}</NotificationCenterProvider>
 		</>
 	)
 }
