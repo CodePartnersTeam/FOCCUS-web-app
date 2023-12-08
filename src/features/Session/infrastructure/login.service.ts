f4409ac4bbf9ec7c55cf3e0f2b8a0f199aedc1a9
DiegoS <sdiegoss@gmail.com>
2023-12-08 18:40:25 +0100

    fix(Login): :bug: se modifico el metodo ,se dejo de usar el interceptor,ahora se utiliza axios

--- src/features/Session/infrastructure/login.service.ts
+++ src/features/Session/infrastructure/login.service.ts
@@ -1,18 +1,19 @@
-import Http from '@/config/interceptors/axios.interceptor'
 import { APIROUTES } from '@/config/routes'
 import { Credentials, credentialsIsValid } from '@/features/_shared/User'
 import { userAdapter } from '@/features/_shared/User/application'
 import { saveUserInLocalStorage } from '@/features/_shared/User/infrastructure'
+import axios from 'axios'
+import { toast } from 'sonner'
 
 import { saveSessionInCookies } from '.'
 import { sessionAdapter } from '../application'
 
 export async function loginService(credentials: Credentials) {
 	if (credentialsIsValid(credentials)) {
-		// TODO: No usar interceptor
-		const { data: sessionDTO } = await Http.post(APIROUTES.LOGIN, credentials)
+		const { data: sessionDTO } = await axios.post(APIROUTES.LOGIN, credentials)
 		saveSessionInCookies(sessionAdapter(sessionDTO))
 		saveUserInLocalStorage(userAdapter(sessionDTO))
+		toast.success(JSON.stringify(`'Bienvenido' ${credentials.username}`))
 		return
 	}
 
