77c09bb9a1ed3484e407b6a06440f2655c034617
DiegoS <sdiegoss@gmail.com>
2023-12-09 18:24:27 +0100

    feat(Login): :zap: se creo un interceptor para el login que pueda manejar los toast
    se modifico el interceptor que se utiliza en el login

--- src/features/Session/infrastructure/login.service.ts
+++ src/features/Session/infrastructure/login.service.ts
@@ -1,19 +1,17 @@
+import HttpLogin from '@/config/interceptors/axios.login'
 import { APIROUTES } from '@/config/routes'
 import { Credentials, credentialsIsValid } from '@/features/_shared/User'
 import { userAdapter } from '@/features/_shared/User/application'
 import { saveUserInLocalStorage } from '@/features/_shared/User/infrastructure'
-import axios from 'axios'
-import { toast } from 'sonner'
 
 import { saveSessionInCookies } from '.'
 import { sessionAdapter } from '../application'
 
 export async function loginService(credentials: Credentials) {
 	if (credentialsIsValid(credentials)) {
-		const { data: sessionDTO } = await axios.post(APIROUTES.LOGIN, credentials)
+		const { data: sessionDTO } = await HttpLogin.post(APIROUTES.LOGIN, credentials)
 		saveSessionInCookies(sessionAdapter(sessionDTO))
 		saveUserInLocalStorage(userAdapter(sessionDTO))
-		toast.success(JSON.stringify(`'Bienvenido' ${credentials.username}`))
 		return
 	}
 
