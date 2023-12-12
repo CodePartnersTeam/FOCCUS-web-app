4093f15321442e3506af042ccbe1652f27308b14
DiegoS <sdiegoss@gmail.com>
2023-12-12 17:52:25 +0100

    refactor(Navigation routes): :recycle: se retiraron de las rutas para el navegador de header
    se retiro , consultations, advisors de app.routes y se retiro del headers navigation

--- src/config/routes/APP.routes.ts
+++ src/config/routes/APP.routes.ts
@@ -4,8 +4,6 @@ export const APPROUTES: KeyWithValue<string> = {
 	LOGIN: '/',
 	HOME: '/vision general',
 	ORDERS: '/pedidos',
-	CONSULTATIONS: '/consultas',
 	PRODUCTS: '/productos',
-	CLIENTS: '/clientes',
-	ADVISORS: '/asesores'
+	CLIENTS: '/clientes'
 }
