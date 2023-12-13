88546bd7a7e4e9ca3940ee593abb835204e072a3
devEstivenValencia <dev@estivenvalencia.co>
2023-12-13 11:37:47 -0500

    build(NotificationCenter): :green_heart: NotificationCenter es del cliente con 'useEffect'

--- src/app/components/NotificationCenter/NotificationCenter.tsx
+++ src/app/components/NotificationCenter/NotificationCenter.tsx
@@ -4,43 +4,46 @@ import { launchNotificacionsToast } from '@/app/components/NotificationCenter/no
 import { getAuthorization } from '@/common/utils'
 import { DINAMICAPIROUTES } from '@/config/routes'
 import { onReceivingMessage, saveNotificationEventsToLS } from '@/features/NotificationsCenter/infrastructure'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SSE } from 'sse.js'
 
+const MAX_RECONNECT_TRIES = 3
+let RECONNECT_ATTEPMS = 0
+
 export function NotificationCenter({ children }: { children: React.ReactNode }) {
-	const MAX_RECONNECT_TRIES = 3
-	let RECONNECT_ATTEPMS = 0
-	const dateOfLastNotification = localStorage.getItem('dateOfLastNotificationEvent')
+	useEffect(() => {
+		const dateOfLastNotification = localStorage.getItem('dateOfLastNotificationEvent')
 
-	const conection = new SSE(DINAMICAPIROUTES.NOTIFICATIONEVENTS(dateOfLastNotification), {
-		headers: { Authorization: getAuthorization() }
-		// debug: true
-	})
+		const conection = new SSE(DINAMICAPIROUTES.NOTIFICATIONEVENTS(dateOfLastNotification), {
+			headers: { Authorization: getAuthorization() }
+			// debug: true
+		})
 
-	conection.onmessage = event => {
-		const { newNotifications, notificationsToSave } = onReceivingMessage(JSON.parse(event.data))
-		const lastnotificationDate = newNotifications[0].timestamp
-		launchNotificacionsToast(newNotifications)
-		saveNotificationEventsToLS(notificationsToSave)
-		localStorage.setItem('dateOfLastNotificationEvent', String(lastnotificationDate))
-	}
-
-	conection.onabort = () => {
-		console.log('Conexion con el servidor cerrada')
-	}
-
-	conection.onerror = () => {
-		if (RECONNECT_ATTEPMS > MAX_RECONNECT_TRIES) {
-			conection.close()
-		} else {
-			console.log('Reconectando...')
-			RECONNECT_ATTEPMS++
+		conection.onmessage = event => {
+			const { newNotifications, notificationsToSave } = onReceivingMessage(JSON.parse(event.data))
+			const lastnotificationDate = newNotifications[0].timestamp
+			launchNotificacionsToast(newNotifications)
+			saveNotificationEventsToLS(notificationsToSave)
+			localStorage.setItem('dateOfLastNotificationEvent', String(lastnotificationDate))
 		}
-	}
 
-	window.addEventListener('beforeunload', () => {
-		conection.close()
-	})
+		conection.onabort = () => {
+			console.log('Conexion con el servidor cerrada')
+		}
+
+		conection.onerror = () => {
+			if (RECONNECT_ATTEPMS > MAX_RECONNECT_TRIES) {
+				conection.close()
+			} else {
+				console.log('Reconectando...')
+				RECONNECT_ATTEPMS++
+			}
+		}
+
+		window.addEventListener('beforeunload', () => {
+			conection.close()
+		})
+	}, [])
 
 	return children
 }
